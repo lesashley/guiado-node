d20257047c26aa2861262af2fc40a242b2421c7f
lesahley <lesashley56@gmail.com>
2017-06-30 18:21:54 -0500

    update server.js: port support

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.set('port', (process.env.PORT || 8080) );
+
 app.use('/static', express.static(path.join(__dirname,'node_modules')));
 app.use('/static', express.static(path.join(__dirname,'assets')));
 app.get('/', (req, res) =>{
   // res.send('Hello world!!')
   res.sendFile(__dirname+'/index.html');
-}).listen(8080);
+});
 
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 /**/
